d6cd08690be67842a0f5b57c026d6bc748edbd90
vahidsediqi <vsediqi@live.com>
2022-03-07 16:39:01 +0300

    shop page

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -511,3 +511,20 @@ document.addEventListener('DOMContentLoaded', function() {
   const pagetitle = document.querySelector('.main-title__primary');
 
   pagetitle.innerHTML = pagetitle.replace('<span>', '')
+
+  // Single prodcut image slider
+
+  const activeImage = document.querySelector(".product-image .active");
+const productImages = document.querySelectorAll(".image-list img");
+const navItem = document.querySelector('a.toggle-nav');
+
+function changeImage(e) {
+  activeImage.src = e.target.src;
+}
+
+function toggleNavigation(){
+  this.nextElementSibling.classList.toggle('active');
+}
+
+productImages.forEach(image => image.addEventListener("click", changeImage));
+navItem.addEventListener('click', toggleNavigation);
